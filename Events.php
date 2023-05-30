6cd714ceda3b260f07ba814706d24797ce1ef58d
Sébastien Mennetrier <mennetrier@smart4.io>
2023-05-30 15:26:48 +0200

    Fix for humhub version >= 1.14.0

--- Events.php
+++ Events.php
@@ -5,6 +5,7 @@ namespace humhub\auth\basic;
 use humhub\components\Request;
 use humhub\modules\user\models\forms\Login;
 use humhub\modules\user\authclient\AuthClientHelpers;
+use humhub\modules\user\services\AuthClientService;
 use yii;
 
 class Events
@@ -25,14 +26,30 @@ class Events
         if ($username != null && $password != null && $identity == null) {
             $login = new Login;
             if ($login->load(['username' => $username, 'password' => $password], '') && $login->validate()) {
-                $user = AuthClientHelpers::getUserByAuthClient($login->authClient);
-                if ($user == null) {
-                    $user = AuthClientHelpers::createUser($login->authClient);
-                }
-                if ($user != null) {
-                    Yii::$app->user->login($user);
+                if (class_exists("AuthClientService")) {
+                    $authClientService = new AuthClientService($login->authClient);
+                    $user = $authClientService->getUser();
+                    if ($user == null) {
+                        $user = $authClientService->createUser($login->authClient);
+                    }
+                    if ($user != null) {
+                        Yii::$app->user->login($user);
+                    }
+                } else {
+                    $user = AuthClientHelpers::getUserByAuthClient($login->authClient);
+                    if ($user == null) {
+                        $user = AuthClientHelpers::createUser($login->authClient);
+                    }
+                    if ($user != null) {
+                        Yii::$app->user->login($user);
+                    }
                 }
             }
+        }
+
+        // Yii::error(Yii::$app->request);
+        if ("user/auth/login" === Yii::$app->request->getPathInfo()) {
+            Yii::$app->getResponse()->redirect(['/user/password-recovery']);
         }
     }
 
